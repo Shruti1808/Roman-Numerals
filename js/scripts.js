69389cf7f0300f6d539f40f15f707d9c49bf74b9
Nicole Sanders and Shruti Priya <nicole@>
2017-01-19 14:04:41 -0800

    add code for conversion of four digit number to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,50 @@
 var romanNumeral = function(aNumber) {
   var symbolArray = ["I", "V", "X", "L", "C", "D", "M"];
   var valueArray = [1, 5, 10, 50, 100, 500, 1000];
-
-  //
-  // for (var symbol = 0; symbol < symbolArray.length; symbol++){
-  //   if (aNumber === valueArray[symbol]){
-  //     result = symbolArray[symbol];
-  //     return result;
-  //   } else if (aNumber ===3){
-  //     result = "I".repeat(3)
-  //     return result;
-  //   };
-  // };
-
-  var result = ""
+  var digitArray = aNumber.split("");
+  var result = "";
+  var tens = "";
+  var hundreds ="";
+  var thousands = ""
+  var ones = ""
 
 
   for(i=0; i<valueArray.length; i++){
-     if (aNumber === valueArray[i] - valueArray[i-1]) {
-       result = symbolArray[i-1] + symbolArray[i];
-       return result;
-     } else if (aNumber === valueArray[i]){
-       result = symbolArray[i];
-       return result;
-     } else if(aNumber < 4){
-       result= "I".repeat(aNumber);
-       return result;
-     } else if (aNumber >= 5 && aNumber < 9) {
-       result = "V" + "I".repeat(aNumber - 5);
-       return result;
-     }
+    if (aNumber === valueArray[i] - valueArray[i-1]) {
+      result = symbolArray[i-1] + symbolArray[i];
+      return result;
+    } else if (aNumber === valueArray[i]){
+      result = symbolArray[i];
+      return result;
+    }
+  }
+  // For thousands and hundreds !
+  if (digitArray.length === 4){
+    thousands = "M".repeat(digitArray[0]);
 
+    if (digitArray[1] < 4 && digitArray[1] >= 1){
+      hundreds = "C".repeat(digitArray[1]);
 
+    }else if (digitArray[1]>=5 && digitArray[1]<9) {
+      hundreds = "D" + "C".repeat(digitArray[1]-5);
+    }
+
+    // for tens and ones !
+    if (digitArray[2]<4 && digitArray[2]>=1){
+      tens= "X".repeat(digitArray[2]);
+    }else if (digitArray[2]>=5 && digitArray[2]<9){
+      tens = "L"+ "X".repeat(digitArray[2]-5);
+    }
+
+    if(digitArray[3] < 4){
+      ones= "I".repeat(digitArray);
+
+    } else if (digitArray[3] >= 5 && digitArray[3] < 9) {
+      ones = "V" + "I".repeat(digitArray[3]- 5);
+
+    }
+
+    return thousands + hundreds + tens + ones;
   }
 };
 
@@ -46,7 +59,7 @@ $(document).ready(function(){
   $("form#roman").submit(function(event){
    event.preventDefault();
 
-   var userInput = parseInt($("#number").val());
+   var userInput = ($("#number").val());
 
    $("#result").text(romanNumeral(userInput));
 
