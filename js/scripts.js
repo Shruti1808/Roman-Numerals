cab41be26ba6279777637045170521e316a43794
Nicole Sanders and Shruti Priya <nicole@>
2017-01-19 10:54:00 -0800

    add conditions for numbers like 4,9,40,400

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,38 @@ var romanNumeral = function(aNumber) {
   var symbolArray = ["I", "V", "X", "L", "C", "D", "M"];
   var valueArray = [1, 5, 10, 50, 100, 500, 1000];
 
-  for (var symbol = 0; symbol < symbolArray.length; symbol++){
-    if (aNumber === valueArray[symbol]){
-      var result = symbolArray[symbol];
-      return result
-    }
+  //
+  // for (var symbol = 0; symbol < symbolArray.length; symbol++){
+  //   if (aNumber === valueArray[symbol]){
+  //     result = symbolArray[symbol];
+  //     return result;
+  //   } else if (aNumber ===3){
+  //     result = "I".repeat(3)
+  //     return result;
+  //   };
+  // };
+
+  var result = ""
+
+
+  for(i=0; i<valueArray.length; i++){
+     if (aNumber === valueArray[i] - valueArray[i-1]) {
+       result = symbolArray[i-1] + symbolArray[i];
+       return result;
+     } else if (aNumber === valueArray[i]){
+       result = symbolArray[i];
+       return result;
+     } else if(aNumber < 4){
+       result= "I".repeat(aNumber);
+       return result;
+     } else if (aNumber >= 5 && aNumber < 9) {
+       result = "V" + "I".repeat(aNumber - 5);
+       return result;
+     }
+
+
   }
-
-
-}
+};
 
 
 
